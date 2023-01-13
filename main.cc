0c866f83c126c2be3a662fb40fcb80826ec070b1
199 <1303339484@qq.com>
2023-01-13 18:57:12 +0800

    aaa

---
+++ main.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<typeinfo>
+using namespace std;
+using namespace cv;
+int main()
+{
+    cv::Mat image = cv::imread("/home/melodic/daily/test/ubuntu.png");
+    
+    cv::Mat x =  Mat::zeros(5,3,CV_32F);
+    
+    cout<<x.size();
+
+  
+    
+   
+}
